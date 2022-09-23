21226b95d74bfc4050662679c9a6c67d7557da91
spaceliftderek <derekm@spacelift.io>
2022-09-23 11:50:10 -0500

    changed vars

--- random_numbers/variables.tf
+++ random_numbers/variables.tf
@@ -1,20 +1,9 @@
 variable "min_number" {
-<<<<<<< HEAD
     type = number
     default = 1
 }
 
 variable "max_number" {
     type = number 
-    default = 26
+    default = 25
 }
-=======
-  type = number
-  default = 1
-}
-
-variable "max_number" {
-  type = number
-  default = 26
-}
->>>>>>> 616e37ddd8f4056aac944b3d61d84319b82b08fa
