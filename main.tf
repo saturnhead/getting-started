f3a9888d213a67deac18a66250681b7ba2446d4d
spaceliftderek <derekm@spacelift.io>
2022-09-23 15:24:16 -0500

    changed to sl module

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ module "numbers" {
   count  = 5
 }
 
-module "passwords" {
-  source = "./random_password"
+# module "passwords" {
+#   source = "./random_password"
+# }
+
+module "random_password" {
+  source  = "spacelift.io/saturnhead/random_password/default"
+  version = "0.1.0"
+
+  # Optional inputs 
+  # password_length           = number
+  # password_override_special = bool
 }
