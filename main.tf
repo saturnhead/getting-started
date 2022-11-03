7388b2f5d68ad3424bf5ac07ed48bbf033ebce81
spaceliftderek <derekm@spacelift.io>
2022-11-03 11:15:32 -0500

    fix file

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
 module "numbers" {
   source = "./random_numbers"
-<<<<<<< HEAD
   count  = 4
-=======
-  count  = 6
->>>>>>> 76b6258 (changed count)
 }
 
 # module "passwords" {
@@ -15,14 +11,7 @@ module "random_password" {
   source  = "spacelift.io/saturnhead/random_password/default"
   version = "0.1.0"
 
-<<<<<<< HEAD
   # Optional inputs 
   # password_length           = number
   # password_override_special = bool
 }
-=======
-  Optional inputs 
-  password_length           = number
-  password_override_special = bool
-}
->>>>>>> 76b6258 (changed count)
