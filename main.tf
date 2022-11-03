b5bfd68cb081f0b22b9062582631f58522ca2acd
spaceliftderek <derekm@spacelift.io>
2022-11-03 11:07:49 -0500

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 module "numbers" {
   source = "./random_numbers"
+<<<<<<< HEAD
   count  = 4
+=======
+  count  = 6
+>>>>>>> 76b6258 (changed count)
 }
 
 # module "passwords" {
@@ -11,7 +15,14 @@ module "random_password" {
   source  = "spacelift.io/saturnhead/random_password/default"
   version = "0.1.0"
 
+<<<<<<< HEAD
   # Optional inputs 
   # password_length           = number
   # password_override_special = bool
 }
+=======
+  Optional inputs 
+  password_length           = number
+  password_override_special = bool
+}
+>>>>>>> 76b6258 (changed count)
